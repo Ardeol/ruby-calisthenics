ed77dea043b1709e42cadaee36e59f75c7bf699b
Auroratide <tf.auroratide@gmail.com>
2016-02-03 21:46:54 +0000

    finished part 1

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,18 +1,26 @@
 module FunWithStrings
   def palindrome?
-    str = self.downcase.gsub(/[^a-z0-9]/, "")
+    str = self.downcase.gsub(/\W/, "")
     return str == str.reverse
   end
   def count_words
-    strs = self.strip.downcase.gsub(/[^a-z0-9 ]/, "").split(/ +/)
+    strs = self.strip.downcase.gsub(/[^\w ]/, "").split(/ +/)
     words = Hash.new(0)
     strs.each do |str|
-      words[str] = words[str] + 1
+      words[str] += 1
     end
     return words
   end
   def anagram_groups
-    # your code here
+    words = self.strip.gsub(/[^\w ]/, "").split(/ +/)
+  # Create a hash whose default is the empty array; you have to do it like the below in Ruby
+    hash = Hash.new{ |h, k| h[k] = [] }
+    words.each do |word|
+    # For each word, convert it into a sorted sequence of characters; every anagram group will have the exact same sequence
+      standard_word = word.downcase.chars.sort.join
+      hash[standard_word] << word
+    end
+    return hash.values
   end
 end
 
