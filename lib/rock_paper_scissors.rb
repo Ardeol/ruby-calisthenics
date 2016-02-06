081d9d07a359a921db56da6833a3031504113251
Auroratide <tf.auroratide@gmail.com>
2016-02-06 16:34:11 +0000

    done

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,24 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    valid_strategies = ["R", "P", "S"]
+    p1 = player1[1]
+    p2 = player2[1]
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless valid_strategies.any? { |strat| p1 == strat } and valid_strategies.any? { |strat| p2 == strat }
+    if (p2 == "R" and p1 == "S") or (p2 == "P" and p1 == "R") or (p2 == "S" and p1 == "P")
+      return player2
+    else
+      return player1
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0].is_a?(String)
+      return tournament
+    end
+    winner1 = tournament_winner(tournament[0])
+    winner2 = tournament_winner(tournament[1])
+    return winner(winner1, winner2)
   end
 
 end
